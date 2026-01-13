cb77c34b229e07050d84b7f68f1ad12bc2a321ba
Rishikesh Negi <rishinegi97@gmail.com>
2026-01-13 20:00:31 +0530

    Created a type for the component's props object and made the component of type FC imported from @types/react package

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,11 +1,41 @@
-export default function CourseGoal() {
-  return (
-    <article>
-      <div>
-        <h2>TITLE</h2>
-        <p>Description</p>
-        <button>Delete</button>
-      </div>
-    </article>
-  );
-}
+// import type { ReactNode } from "react";
+import type { FC, PropsWithChildren } from "react";
+
+// Defining a type for the component's props object:
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+// Alternative way:
+type CourseGoalProps = PropsWithChildren<{
+  title: string;
+  onDelete: (id: number) => void;
+  id: number;
+}>;
+
+// export default function CourseGoal({ title, children }: CourseGoalProps) {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//         <button>Delete</button>
+//       </div>
+//     </article>
+//   );
+// }
+
+// A concise alternative syntax for writing React functional components:
+const CourseGoal: FC<CourseGoalProps> = ({ title, onDelete, id, children }) => (
+  <article>
+    <div>
+      <h2>{title}</h2>
+      {children}
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </div>
+  </article>
+);
+// FC, PropsWithChildren, and ReactNode are built-in generic types in the "@types/react" package
+
+export default CourseGoal;
